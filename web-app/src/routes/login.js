2d0e3b6f6073ec2aa8841c5dfdef47e484d4ea2e
Rusith <rusith@calcey.com>
2018-11-26 18:58:07 +0530

    Added Go

--- web-app/src/routes/login.js
+++ web-app/src/routes/login.js
@@ -3,15 +3,26 @@ import React, { useState } from 'react'
 import {
   Button,
   Form,
-  Segment
+  Segment,
+  Message
 } from 'semantic-ui-react'
 
 export default () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  function handleLogInClick() {
+    if (!username || !password) {
+      return setErrorMessage('Username and password are required')
+    }
+
+    return null
+  }
+
   return (
     <div className="login">
-      <Segment raised>
+      <Segment raised id="form-segment">
         <Form>
           <Form.Field>
             <label htmlFor="username">Username</label>
@@ -19,9 +30,16 @@ export default () => {
           </Form.Field>
           <Form.Field>
             <label htmlFor="password">Password</label>
-            <input placeholder="Password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="password" placeholder="Password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
           </Form.Field>
-          <Button type="submit">Log In</Button>
+
+          {!!errorMessage && (
+            <Message negative id="error-message">
+              <p>{errorMessage}</p>
+            </Message>
+          )}
+          <Button primary onClick={handleLogInClick}>Log In</Button>
+          <Button>Sign Up</Button>
         </Form>
       </Segment>
     </div>
--- web-app/src/types/auth.js
+++
@@ -1,2 +0,0 @@
-export const LOG_IN_STARTED = 'LOG_IN_STARTED'
-export const LOG_IN_FAILED = 'LOG_IN_FAILED'
